c5549f7a2692acaca11a276de34774e69be2d9fb
Brandon Criss <bcriss@esi-estech.com>
2019-04-23 21:11:28 -0500

    Initial commit

---
+++ Smack/Controllers/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Brandon Criss on 4/23/19.
+//  Copyright © 2019 Brandon Criss. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+}
