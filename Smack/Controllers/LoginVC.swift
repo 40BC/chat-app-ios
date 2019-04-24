0afe59298b78d71df9aacc31c50c7470bb632601
Brandon Criss <bcriss@esi-estech.com>
2019-04-23 22:31:00 -0500

    LoginVC UI

---
+++ Smack/Controllers/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Brandon Criss on 4/23/19.
+//  Copyright © 2019 Brandon Criss. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Brandon Criss on 4/23/19.
+//  Copyright © 2019 Brandon Criss. All rights reserved.
+//
+
+import Foundation
+
+//
+let TO_LOGIN = "toLogin"
