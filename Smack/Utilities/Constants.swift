09ec2bb6da1b4062cb6feb63e3d1aec5e3ec65a0
Brandon Criss <bcriss@esi-estech.com>
2019-04-24 20:47:26 -0500

    Creating web request in Xcode

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
-//
+// makes type CompletionHandler type Success which is a bool
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// userDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// networking
+let API_URL = "https://bc40-chat.herokuapp.com/v1"
+let REGISTER_URL = "\(API_URL)/account/register"
