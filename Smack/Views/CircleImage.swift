f68e6a535939d325331ca7ea1ff3e3f52caf3a63
Brandon Criss <bcriss@esi-estech.com>
2019-05-07 21:51:17 -0500

    Generated avatar background color

---
+++ Smack/Views/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Brandon Criss on 5/7/19.
+//  Copyright © 2019 Brandon Criss. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+    
+}
